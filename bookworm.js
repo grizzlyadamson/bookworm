95e6329fa6e1c677183f898f75ecc259da7a9d19
Sam Adamson <grizzlyadamson@gmail.com>
2014-03-02 19:50:19 -0600

    update march leaders

--- bookworm.js
+++ bookworm.js
@@ -1,10 +1,10 @@
 if (Meteor.isClient) {
 
   Template.leaders.people = [
-    { fullName: "Eric Broulette", totalCredits: "9", },
-    { fullName: "Sara Breen", totalCredits: "8" },
-    { fullName: "Todd Prauner", totalCredits: "8" },
-    { fullName: "Hannah Newberry", totalCredits: "7" },
-    { fullName: "Erik Pulverenti", totalCredits: "6" }
+    { fullName: "Todd Parauner", totalCredits: "16", },
+    { fullName: "Eric Broulette", totalCredits: "15" },
+    { fullName: "Sara Breen", totalCredits: "13" },
+    { fullName: "Hannah Newberry", totalCredits: "11" },
+    { fullName: "Jason West", totalCredits: "9" }
   ];
 }
