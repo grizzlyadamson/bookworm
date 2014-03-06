c3ecc74cbe99525f8059e5f00409a2626bda1656
Sam Adamson <sam.adamson@hudl.com>
2014-03-06 10:55:05 -0600

    Fix Todd's name

--- bookworm.js
+++ bookworm.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
 
   Template.leaders.people = [
-    { fullName: "Todd Parauner", totalCredits: "16", },
+    { fullName: "Todd Prauner*", totalCredits: "16", },
     { fullName: "Eric Broulette", totalCredits: "15" },
     { fullName: "Sara Breen", totalCredits: "13" },
     { fullName: "Hannah Newberry", totalCredits: "11" },
